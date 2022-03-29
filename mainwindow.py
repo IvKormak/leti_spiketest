4334ed6add620ad8ae54371de25820b183d70248
IvKormak <kormak.arth@gmail.com>
2022-03-29 18:21:19 +0300

    Добавлена маска весов

--- mainwindow.py
+++ mainwindow.py
@@ -11,16 +11,20 @@ from PySide6.QtCore import QFile, QTimer, QDir, Qt, QObject, QThread, Signal
 from PySide6.QtGui import QPixmap, QImage, qRgb
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QComboBox, QSpinBox, QFileDialog, QCheckBox, \
-    QVBoxLayout, QScrollArea, QSpacerItem
+    QVBoxLayout, QScrollArea, QSpacerItem, QHBoxLayout
 
 import AERGen as ag
 import configmanager as cm
 import main
 
+from checkablecombobox import CheckableComboBox as CComboBox
+
 
 class MainWindow(QWidget):
     startTraining = Signal(list)
     endTraining = Signal()
+    startReckon = Signal()
+    endReckon = Signal()
 
     def __init__(self):
         super(MainWindow, self).__init__()
@@ -35,9 +39,8 @@ class MainWindow(QWidget):
 
         self.path = QDir(os.fspath(Path(__file__).resolve().parent))
 
-
         self.chosenSets = []
-        self.chosenReconSets = []
+        self.chosenReckonSets = []
         self.chosenToRemoveSets = []
 
         self.db = cm.DBManager()
@@ -75,19 +78,28 @@ class MainWindow(QWidget):
         self.setsContainer = self.findChild(QVBoxLayout, "setsContainer")
         self.setsLayout = QVBoxLayout()
 
+        self.hl5 = self.findChild(QHBoxLayout, "horizontalLayout_5")
+        self.datasetCombo_2 = CComboBox()
+        #self.datasetCombo_2 = self.findChild(QComboBox, "datasetCombo_2")
+        self.hl5.insertWidget(0, self.datasetCombo_2)
+
         self.chooseModel = self.findChild(QPushButton, "chooseModel")
         self.chooseModel.clicked.connect(self._chooseModel)
         self.deleteSets = self.findChild(QPushButton, "deleteSets")
-        self.reconSetsLayout = QVBoxLayout()
-        self.reconSetsContainer = self.findChild(QVBoxLayout, "reconSetsContainer")
-        self.reconSetsInnerContainer = QScrollArea()
-        self.reconSetsContainer.insertWidget(0, self.reconSetsInnerContainer)
+        self.reckonSetsLayout = QVBoxLayout()
+        self.reckonSetsContainer = self.findChild(QVBoxLayout, "reckonSetsContainer")
+        self.reckonSetsInnerContainer = QScrollArea()
+        self.reckonSetsContainer.insertWidget(0, self.reckonSetsInnerContainer)
         self.deleteSets.clicked.connect(self._deleteSets)
         self.addSet = self.findChild(QPushButton, "addSet")
         self.addSet.clicked.connect(self._addSet)
-        self.datasetCombo_2 = self.findChild(QComboBox, "datasetCombo_2")
-        self.answers = self.findChild(QScrollArea, "answers")
-        self.recon = self.findChild(QLabel, "recon")
+        self.answers = self.findChild(QLabel, "answers")
+        self.reckonGraphics = self.findChild(QLabel, "reckonGraphics")
+        self.reckon = self.findChild(QPushButton, "reckon")
+        self.reckon.setEnabled(False)
+        self.reckonPixmap = None
+
+        self.reckon.clicked.connect(self._startReckon)
 
         self.loadTraces()
 
@@ -109,6 +121,8 @@ class MainWindow(QWidget):
         self.renderTimer = None
 
         self.colortable = [qRgb(i, i, i) for i in range(256)]
+
+
     def load_ui(self):
         loader = QUiLoader()
         path = os.fspath(Path(__file__).resolve().parent / "form.ui")
@@ -136,55 +150,94 @@ class MainWindow(QWidget):
         self.setStatus('Готово')
 
     def _addSet(self):
-        self.chosenReconSets.append(self.db.get_trace_by_path(self.datasetCombo_2.currentData()))
-        self.renderReconSets()
+        for path in self.datasetCombo_2.currentData():
+            self.chosenReckonSets.append(self.db.get_trace_by_path(path))
+        self.renderReckonSets()
 
     def _deleteSets(self):
-        self.chosenReconSets = [i for i in self.chosenReconSets if i.trace_path not in self.chosenToRemoveSets]
-        self.renderReconSets()
+        self.chosenReckonSets = [i for i in self.chosenReckonSets if i.trace_path not in self.chosenToRemoveSets]
+        self.renderReckonSets()
 
     def _chooseModel(self):
-        with open(self.path.relativeFilePath(
-                QFileDialog.getOpenFileName(self, "Выберите файл с обученной моделью", "")[0]), "rb") as f:
-            self.model_file = pickle.load(f)
+        p = QFileDialog.getOpenFileName(self, "Выберите файл с обученной моделью", filter="*.pkl")
+        if any(p):
+            with open(self.path.relativeFilePath(p[0]), "rb") as f:
+                self.reckonModel = pickle.load(f)
+        self.reckon.setEnabled(True)
 
-    def fillLabels(self):
-        pass
+    def fillLabels(self, labels:dict):
+        s = ""
+        for k in labels.keys():
+            s += f"{k}\n"
+        self.answers.setText(s)
 
-    def renderReconSets(self):
-        self.reconSetsContainer.removeWidget(self.reconSetsInnerContainer)
-        self.reconSetsLayout = QVBoxLayout()
+    def renderReckonImage(self, events):
+        if self.reckonPixmap is None:
+            white_square = np.ones((28, 28), dtype=np.uint8)
+            self.reckonPixmap = np.copy(white_square)
+        for ev in events:
+            x_coord = ev.position.x
+            y_coord = ev.position.y
+            color = ev.polarity * 255
+            if 0 <= x_coord < 28 and 0 <= y_coord < 28:
+                self.reckonPixmap[y_coord][x_coord] = color
+            img = QImage(self.reckonPixmap, 28, 28, 28, QImage.Format_Indexed8)
+            img.setColorTable(self.colortable)
+            pixmap = QPixmap(img).scaledToHeight(300)
+            self.reckonGraphics.setPixmap(pixmap)
 
-        for trace in self.chosenReconSets:
+    def renderReckonSets(self):
+        self.reckonSetsContainer.removeWidget(self.reckonSetsInnerContainer)
+        self.reckonSetsLayout = QVBoxLayout()
+
+        for trace in self.chosenReckonSets:
             button = QCheckBox(trace.trace_alias)
             button.stateChanged.connect(self.toggleDataSet(trace.trace_path, self.chosenToRemoveSets))
-            self.reconSetsLayout.insertWidget(0, button)
-        self.reconSetsLayout.addStretch()
+            self.reckonSetsLayout.insertWidget(0, button)
+        self.reckonSetsLayout.addStretch()
 
         widget = QWidget()
-        widget.setLayout(self.reconSetsLayout)
+        widget.setLayout(self.reckonSetsLayout)
 
-        self.reconSetsInnerContainer = QScrollArea()
-        self.reconSetsInnerContainer.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
-        self.reconSetsInnerContainer.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.reconSetsInnerContainer.setWidgetResizable(True)
-        self.reconSetsInnerContainer.setWidget(widget)
+        self.reckonSetsInnerContainer = QScrollArea()
+        self.reckonSetsInnerContainer.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.reckonSetsInnerContainer.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.reckonSetsInnerContainer.setWidgetResizable(True)
+        self.reckonSetsInnerContainer.setWidget(widget)
 
-        self.reconSetsContainer.insertWidget(0, self.reconSetsInnerContainer)
+        self.reckonSetsContainer.insertWidget(0, self.reckonSetsInnerContainer)
 
+    def _startReckon(self):
+        self.reckonthread = QThread()
+        self.reckonthread.start()
+        self.reckognizer = ReckognizerWorker(self.reckonModel, self.chosenReckonSets)
+        self.reckognizer.moveToThread(self.reckonthread)
+        self.reckognizer.done.connect(self._endReckon)
+        self.reckognizer.events.connect(self.renderReckonImage)
+        self.reckognizer.events.connect(self.update)
+        self.reckognizer.reckon.connect(self.fillLabels)
+        self.endReckon.connect(self.reckonthread.quit)
+        self.startReckon.connect(self.reckognizer.reckognize_frame)
+        self.startReckon.emit()
+
+    def _endReckon(self):
+        self.reckonPixmap = None
+        self.endReckon.emit()
+        del self.reckognizer
+        del self.reckonthread
 
     def _datasetDelete(self):
         self.db.delete_trace_entry(self.datasetCombo.currentData())
         self.loadTraces()
 
     def toggleDataSet(self, alias, variable):
-        def _(state):
+        def toggle(state):
             if not state:
                variable.remove(alias)
             else:
                 variable.append(alias)
 
-        return _
+        return toggle
 
     def _datasetPreview(self):
         self.renderPreview()
@@ -194,7 +247,6 @@ class MainWindow(QWidget):
         self.renderPreview()
 
     def generateDataset(self, time=0):
-        print(1)
         self.setStatus('Начинаем генерацию траектории')
         self.dataset = []
         raw_dataset = ag.AERGen(radius=self.newDatasetInputs["radius"].value(),
@@ -213,14 +265,13 @@ class MainWindow(QWidget):
         self.renderTimer = QTimer(self)
         self.renderTimer.timeout.connect(self.showNextFrame)
         self.renderTimer.timeout.connect(self.update)
-        self.genNextFrame = self._genNextFrame()
         self.renderComplete = False
         self.showNextFrame()
 
     def showNextFrame(self):
         if not self.renderComplete:
             try:
-                self.preview.setPixmap(next(self.genNextFrame))
+                self.preview.setPixmap(next(self._genNextFrame))
             except StopIteration:
                 self.renderComplete = True
                 self.setStatus('Готово')
@@ -234,10 +285,8 @@ class MainWindow(QWidget):
             x_coord = ev.position.x
             y_coord = ev.position.y
             color = ev.polarity * 255
-            try:
+            if 0 <= x_coord < 28 and 0 <= y_coord < 28:
                 current_frame[y_coord][x_coord] = color
-            except:
-                pass
             frames_shown += 1
             img = QImage(current_frame, 28, 28, 28, QImage.Format_Indexed8)
             img.setColorTable(self.colortable)
@@ -279,7 +328,6 @@ class MainWindow(QWidget):
         self.network_conf = self.path.relativeFilePath(
             QFileDialog.getOpenFileName(self, "Выберите файл с конфигурацией модели", "")[0])
 
-    # noinspection PyUnresolvedReferences
     def _letsGo(self):
         self.timestart = time.time()
         if not self.network_conf:
@@ -316,7 +364,6 @@ class MainWindow(QWidget):
         self.startTraining.emit(self.chosenSets)
         self.setStatus('Начинаем обучение')
 
-    # noinspection PyUnresolvedReferences
     def _trainingFinished(self, donor_model):
         self.donorModels.append(donor_model)
         self._poolSize -= 1
@@ -339,11 +386,16 @@ class MainWindow(QWidget):
 
                 self.trainResult.setPixmap(pixmap)
                 self.letsGo.setEnabled(True)
+
+                del self.threads
+                del self.trainers
+
+                self.threads = []
+                self.trainers = []
             else:
                 self._poolSize = self.poolSize.value()
                 self.startTraining.emit(self.chosenSets)
                 self.setStatus('Начинаем обучение заново')
-
 
 
 class TrainerWorker(QObject):
@@ -353,7 +405,6 @@ class TrainerWorker(QObject):
         self.model, self.feed = main.construct_network(**kwargs)
         super().__init__()
 
-    # noinspection PyUnresolvedReferences
     def train(self, chosenSets):
         main.reset(self.model, self.feed)
         datasets = []
@@ -377,15 +428,50 @@ class TrainerWorker(QObject):
 
 
 class ReckognizerWorker(QObject):
-    reckon = Signal(str)
+    reckon = Signal(dict)
+    events = Signal(list)
+    done = Signal()
 
-    def __init__(self, source):
+    def __init__(self, model, chosenSets):
         super(ReckognizerWorker, self).__init__()
-        self.model = main.load_network(source)
+        self.reckonTimer = QTimer(self)
+        self.time = 0
+        self.timewindow = 10
+        self.fps = 50
+        self.neuron_signals = {}
+        self.datasets = []
+        self.model = model
+        for layer in self.model.layers:
+            for neuron in layer["neurons"]:
+                neuron.learn = False
+        for path in chosenSets:
+            with open(path.trace_path, 'r') as f:
+                self.datasets.append((path, [ag.aer_decode(ev) for ev in f.readline().split(' ')]))
+        self.feed = main.DataFeed("iter", self.model)
+        main.reset(self.model, self.feed, issoft=True)
+        self.feed.load(*self.datasets.pop())
+        self.reckonTimer.timeout.connect(self.reckognize_frame)
 
-    # noinspection PyUnresolvedReferences
-    def reckognize(self, events):
-        pass
+        self.lut = {synapse:
+                        [neuron.label for neuron in self.model.layers[-1]["neurons"]
+                         if neuron.output_address == synapse][0]
+                    for synapse in self.model.outputs}
+
+    def reckognize_frame(self):
+        self.events.emit(self.feed.next_events(peek=True))
+        fired_neurons = [self.lut[synapse] for synapse in self.model.outputs if self.model.state[synapse]]
+        for label in fired_neurons:
+            self.neuron_signals[label] = self.time
+        self.neuron_signals = {k:v for k,v in self.neuron_signals.items() if v > self.time-self.timewindow}
+        self.reckon.emit(self.neuron_signals)
+        self.time += 1
+        if not main.next_recognition_cycle(self.model, self.feed):
+            if len(self.datasets):
+                self.feed.load(*self.datasets.pop())
+            else:
+                self.done.emit()
+                return
+        self.reckonTimer.start(1000/self.fps)
 
 
 if __name__ == "__main__":
